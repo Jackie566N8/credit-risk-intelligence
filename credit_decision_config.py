80e39a5729b4b724b4d8d3dfde10da4f64935079
Jackie566N8 <2944978947@qq.com>
2026-01-25 12:48:14 +0800

    Add model config.

---
+++ credit_decision_config.py
@@ -0,0 +1,20 @@
+PURPOSE_OPTIONS = [
+    "debt_consolidation",
+    "credit_card",
+    "home_improvement",
+    "major_purchase",
+    "small_business",
+    "medical",
+    "car",
+    "other",
+]
+
+HOME_OWNERSHIP_OPTIONS = ["MORTGAGE", "RENT", "OWN", "OTHER"]
+VERIFICATION_OPTIONS = ["Not Verified", "Source Verified", "Verified"]
+TERM_OPTIONS = [36, 60]
+
+BASE_LOSS_GIVEN_DEFAULT = 0.55
+APPROVAL_PD_THRESHOLD = 0.18
+REVIEW_PD_THRESHOLD = 0.35
+APPROVAL_SCORE_THRESHOLD = 660
+REVIEW_SCORE_THRESHOLD = 580
---
+++ figure_gallery_config.py
@@ -0,0 +1,22 @@
+FIGURE_GROUPS = {
+    "Accepted Loans": [
+        "target_count.png",
+        "grade_default_rate.png",
+        "fico_band_default_rate.png",
+        "term_grade_default_rate.png",
+        "income_loan_amount_default_heatmap.png",
+        "purpose_default_rate.png",
+        "interest_rate_by_grade.png",
+        "revol_util_by_target.png",
+        "fico_dti_by_target.png",
+        "numeric_correlation_heatmap.png",
+        "numeric_distributions.png",
+        "loan_amount_by_target.png",
+    ],
+    "Rejected Applications": [
+        "rejected_risk_score_distribution.png",
+        "rejected_amount_vs_dti.png",
+        "top_rejected_states.png",
+        "rejected_applications_by_year.png",
+    ],
+}
